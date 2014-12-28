af08b9a364cc9d015bc5e0af87b2b3483293c48c
Raphaël Benitte <benitteraphael@gmail.com>
2014-12-28 14:30:19 +0100

    Update sensu client

--- src/ext/sensu/client.js
+++ src/ext/sensu/client.js
@@ -3,29 +3,23 @@ require('superagent-bluebird-promise');
 var _       = require('lodash');
 var config  = require('./../../../config');
 
-function makeApiCall(endPoint) {
-    return request.get(config.api.sensu.baseUrl + '/api/' + endPoint)
-        .auth(config.api.sensu.auth.user, config.api.sensu.auth.password)
-        .promise()
-        .then(function (res) {
-            return res.body;
-        })
-        .catch(function (err) {
-            console.log(err);
-        })
-    ;
+function buildEndpointCall(endPoint) {
+    return function () {
+        return request.get(config.api.sensu.baseUrl + '/api/' + endPoint)
+            .auth(config.api.sensu.auth.user, config.api.sensu.auth.password)
+            .promise()
+            .then(function (res) {
+                return res.body;
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+        ;
+    }
 }
 
 module.exports = {
-    clients: function () {
-        return makeApiCall('clients');
-    },
-
-    checks: function () {
-        return makeApiCall('checks');
-    },
-
-    events: function () {
-        return makeApiCall('events');
-    }
+    clients: buildEndpointCall('clients'),
+    checks:  buildEndpointCall('checks'),
+    events:  buildEndpointCall('events')
 };
