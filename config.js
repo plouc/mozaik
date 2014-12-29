54604f2990cf2720e6767cf33b0bd76a08657e74
Raphaël Benitte <benitteraphael@gmail.com>
2014-12-29 11:12:20 +0100

    Add empty config for testing purpose

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host: 'localhost',
+    port: 4000,
+    api: {
+    },
+    rotationDuration: 8000,
+    dashboards: [
+    ]
+};
---
+++ gulp/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    src:  './src/',
+    dest: './build/'
+};
