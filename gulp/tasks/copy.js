4be97c3c4805ac30c227ca364cf7049e3fc955ee
Raphaël Benitte <benitteraphael@gmail.com>
2014-12-31 13:20:26 +0100

    Fix missing icons

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,8 +1,16 @@
 var gulp   = require('gulp');
 var config = require('../config');
 
-gulp.task('copy', function () {
+gulp.task('copy:fonts', function () {
     return gulp.src('./node_modules/font-awesome/fonts/*')
         .pipe(gulp.dest(config.dest + 'fonts'))
     ;
 });
+
+gulp.task('copy:styles', function () {
+    return gulp.src('./node_modules/font-awesome/css/font-awesome.min.css')
+        .pipe(gulp.dest(config.dest))
+    ;
+});
+
+gulp.task('copy', ['copy:fonts', 'copy:styles']);
