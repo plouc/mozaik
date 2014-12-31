0d4fbf9ec20b9b55daa1b20ec8e3720c719fb3ca
Raphaël Benitte <benitteraphael@gmail.com>
2014-12-31 12:44:37 +0100

    Update sample config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: 'localhost',
-    port: 4000,
+    port: process.env.PORT || 5000,
     api: {
         aws: {
             region: 'eu-west-1'
@@ -16,7 +16,7 @@ module.exports = {
             //token: 'my-github-token'
         }
     },
-    rotationDuration: 8000,
+    rotationDuration: 4000,
     dashboards: [
         {
             columns: 3,
@@ -32,7 +32,7 @@ module.exports = {
                     type: 'github.pull_requests',
                     repository: 'plouc/go-gitlab-client',
                     columns: 1, rows: 1,
-                    x: 1, y: 0
+                    x: 1, y: 1
                 },
                 {
                     type: 'travis.repository',
@@ -46,7 +46,7 @@ module.exports = {
                     owner: 'plouc',
                     repository: 'mozaik',
                     columns: 1, rows: 1,
-                    x: 1, y: 1
+                    x: 1, y: 0
                 },
                 {
                     type: 'travis.build_history',
@@ -54,6 +54,39 @@ module.exports = {
                     repository: 'mozaik',
                     columns: 1, rows: 2,
                     x: 2, y: 0
+                }
+            ]
+        },
+        {
+            columns: 3,
+            rows:    2,
+            widgets: [
+                {
+                    type: 'travis.build_history',
+                    owner: 'plouc',
+                    repository: 'mozaik',
+                    columns: 1, rows: 2,
+                    x: 0, y: 0
+                },
+                {
+                    type: 'github.user_badge',
+                    user: 'plouc',
+                    columns: 1, rows: 1,
+                    x: 2, y: 0
+                },
+                {
+                    type: 'travis.repository',
+                    owner: 'plouc',
+                    repository: 'mozaik',
+                    columns: 1, rows: 1,
+                    x: 1, y: 0
+                },
+                {
+                    type: 'travis.build_histogram',
+                    owner: 'plouc',
+                    repository: 'mozaik',
+                    columns: 2, rows: 1,
+                    x: 1, y: 1
                 }
             ]
         }
