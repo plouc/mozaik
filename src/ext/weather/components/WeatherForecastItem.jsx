ca51a4d926c96c9f114e01f83f697b50d884fa8f
Raphaël Benitte <benitteraphael@gmail.com>
2015-01-02 04:35:31 +0100

    Move Weather widget layout to css flexbox

--- src/ext/weather/components/WeatherForecastItem.jsx
+++ src/ext/weather/components/WeatherForecastItem.jsx
@@ -7,11 +7,11 @@ var WeatherForecastItem = React.createClass({
                 <span className="weather__weather__forecast__item__description">{this.props.data.weather[0].description}</span>
                 <span className="weather__weather__forecast__item__min">
                     min.<br />
-                    {Math.round(this.props.data.temp.min - 273.15)} °C
+                    {Math.round(this.props.data.temp.min - 273.15)}°C
                 </span>
                 <span className="weather__weather__forecast__item__max">
                     max.<br />
-                    {Math.round(this.props.data.temp.max - 273.15)} °C
+                    {Math.round(this.props.data.temp.max - 273.15)}°C
                 </span>
             </div>
         );
