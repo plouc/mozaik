0d32fd439d6ed361f9312f9671e45728bd9b14ec
Raphaël Benitte <benitteraphael@gmail.com>
2015-01-25 21:49:48 +0100

    Add health report description on jenkins view widget #9

--- src/ext/jenkins/components/ViewJob.jsx
+++ src/ext/jenkins/components/ViewJob.jsx
@@ -2,9 +2,15 @@ var React = require('react');
 
 var ViewJob = React.createClass({
     render() {
+        var healthReportNode = <td></td>;
+        if (this.props.job.healthReport.length > 0) {
+            healthReportNode = <td className="table__cell">{this.props.job.healthReport[0].description}</td>
+        }
+
         return (
             <tr>
-                <td>{this.props.job.displayName}</td>
+                <td className="table__cell">{this.props.job.displayName}</td>
+                {healthReportNode}
             </tr>
         );
     }
