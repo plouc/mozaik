0f787a4bad4020993bac2e6c42563131b8e91e3e
Raphaël Benitte <benitteraphael@gmail.com>
2015-01-28 01:35:22 +0100

    Add ability to make twitter hashtag aggregation with twitter client

--- src/ext/twitter/client.js
+++ src/ext/twitter/client.js
@@ -1,5 +1,6 @@
 var Twitter = require('twitter');
 var Promise = require('bluebird');
+var _       = require('lodash');
 var config  = require('./../../../config');
 
 var _client;
@@ -18,10 +19,12 @@ function getClient() {
 }
 
 
-function _get(op) {
+function _get(op, q) {
     var def = Promise.defer();
 
-    getClient().get(op, function (err, params, response) {
+    q = q || {};
+
+    getClient().get(op, q, function (err, params, response) {
         if (!err) { def.resolve(params); }
         else      { def.reject(err);     }
     });
@@ -33,5 +36,47 @@ function _get(op) {
 module.exports = {
     userTimeline: function () {
         return _get('statuses/user_timeline');
+    },
+    searchByHashtags: function (params) {
+        var hashtagsText     = [];
+        var hashtagsNormText = [];
+
+        params.hashtags.forEach(function (hashtag) {
+            hashtag.normText = hashtag.text.toLowerCase();
+            hashtag.count    = 0;
+
+            hashtagsText.push(hashtag.text);
+            hashtagsNormText.push(hashtag.normText);
+        });
+
+        return _get('search/tweets', {
+            q: '#' + hashtagsText.join(' OR #')
+        })
+            .then(function (res) {
+                var startDate = _.last(res.statuses).created_at;
+                var endDate   = _.first(res.statuses).created_at;
+
+                // Build aggregation by tag text
+                res.statuses.forEach(function (tweet) {
+                    tweet.entities.hashtags.forEach(function (hashtag) {
+                        var normText = hashtag.text.toLowerCase();
+                        if (_.indexOf(hashtagsNormText, normText) > -1) {
+                            _.find(params.hashtags, { normText: normText }).count += 1;
+                        }
+                    });
+                });
+
+                return {
+                    hashtags:  params.hashtags,
+                    dateRange: {
+                        start: startDate,
+                        end:   endDate
+                    }
+                };
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+        ;
     }
 };
