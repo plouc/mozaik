8ce9ba46a08eca23f14d74e4f76315b9787cd78f
Raphaël Benitte <benitteraphael@gmail.com>
2015-03-01 11:15:45 +0100

    Fix hard-coded theme name in gulp task #32

--- gulp/tasks/styl.js
+++ gulp/tasks/styl.js
@@ -1,18 +1,20 @@
-var gulp   = require('gulp');
-var stylus = require('gulp-stylus');
-var gutil  = require('gulp-util');
-var chalk  = require('chalk');
-var config = require('../config');
+var gulp      = require('gulp');
+var path      = require('path');
+var stylus    = require('gulp-stylus');
+var gutil     = require('gulp-util');
+var chalk     = require('chalk');
+var config    = require('../config');
+var appConfig = require(path.join(config.root, 'config.js'));
 
 gulp.task('styl', ['collect:styl'], function () {
-    gutil.log(chalk.green('Compiling stylus code'));
+    gutil.log(chalk.green('Compiling stylus code using theme \'' + appConfig.theme + '\''));
 
     return gulp
         .src(config.mozaikLib + 'styl/mozaik.styl')
         .pipe(stylus({
             'include css': true,
             use: function (style) {
-                style.define('$theme', 'night-blue');
+                style.define('$theme', appConfig.theme);
             }
         }))
         .pipe(gulp.dest(config.dest + 'css'))
