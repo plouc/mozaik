2913f9b467e2a973b3da84f5629f9bd67d343c12
Raphaël Benitte <benitteraphael@gmail.com>
2015-03-02 13:57:39 +0100

    Add ability to set a custom theme #32

--- gulp/tasks/styl.js
+++ gulp/tasks/styl.js
@@ -1,21 +1,32 @@
-var gulp      = require('gulp');
-var path      = require('path');
-var stylus    = require('gulp-stylus');
-var gutil     = require('gulp-util');
-var chalk     = require('chalk');
-var config    = require('../config');
+var gulp   = require('gulp');
+var path   = require('path');
+var stylus = require('gulp-stylus');
+var fs     = require('fs');
+var gutil  = require('gulp-util');
+var chalk  = require('chalk');
+var config = require('../config');
 
-gulp.task('styl', ['collect:styl'], function () {
+gulp.task('styl', ['collect:styl'], function (done) {
     var appConfig = require(path.join(config.root, 'config.js'));
+    var theme = appConfig.theme;
 
-    gutil.log(chalk.green('Compiling stylus code using theme \'' + appConfig.theme + '\''));
+    var mozaikThemePath = config.mozaikLib + path.join('themes', theme);
+    var customThemePath = config.root + path.join('themes', theme);
+
+    if (fs.existsSync(customThemePath)) {
+        theme = customThemePath;
+    } else if (fs.existsSync(mozaikThemePath)) {
+        theme = mozaikThemePath;
+    }
+
+    gutil.log(chalk.green('Compiling stylus code using theme \'' + theme + '\''));
 
     return gulp
         .src(config.mozaikLib + 'styl/mozaik.styl')
         .pipe(stylus({
             'include css': true,
             use: function (style) {
-                style.define('$theme', appConfig.theme);
+                style.define('$theme', theme);
             }
         }))
         .pipe(gulp.dest(config.dest + 'css'))
