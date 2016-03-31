5da5bfe0b1f01d728e07b351ec7580a2f348aa52
Vilhjalmur Gunnarsson <villi@tmsoftware.is>
2016-03-31 14:21:09 +0000

    Add optional title to dashboard

--- src/browser/components/Dashboard.jsx
+++ src/browser/components/Dashboard.jsx
@@ -24,17 +24,26 @@ var Dashboard = React.createClass({
     },
 
     render() {
-        var columns = this.props.dashboard.columns;
-        var rows    = this.props.dashboard.rows;
+        var columns        = this.props.dashboard.columns;
+        var rows           = this.props.dashboard.rows;
+        var title          = this.props.dashboard.title || '';
+        var titleHeight    = 10;
 
         var widgetNodes = _.map(this.props.dashboard.widgets, (widget, index) => {
+            var widgetHeight = widget.rows / rows * 100 + '%';
+            var widgetY = widget.y / rows * 100 + '%';
+            if (title) {
+              widgetHeight = 'calc(' + widgetHeight + ' - ' + (titleHeight / rows) + 'vmin)';
+              widgetY = 'calc(' + widgetY + ' + ' + (titleHeight / (widget.y === 0 ? 1 : rows)) + 'vmin)';
+            }
+
             var props = _.extend({}, _.omit(widget, ['columns', 'rows']), {
                 key:  index,
                 type: widget.type,
                 w:    (widget.columns / columns * 100) + '%',
-                h:    (widget.rows    / rows    * 100) + '%',
+                h:    widgetHeight,
                 x:    (widget.x       / columns * 100) + '%',
-                y:    (widget.y       / rows    * 100) + '%'
+                y:    widgetY
             });
 
             return React.createElement(Widget, props);
@@ -45,8 +54,17 @@ var Dashboard = React.createClass({
             cssClasses += ' _is-current';
         }
 
+        var titleNode = null;
+
+        if (title) {
+          titleNode = (
+            <div className="dashboard__title">{title}</div>
+          );
+        }
+
         return (
             <div className={cssClasses}>
+                {titleNode}
                 {widgetNodes}
             </div>
         );
