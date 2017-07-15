8d1151673aaa78c82b0764d9b12a4746b0680213
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-07-15 13:51:57 +0900

    feat(themes): move themes to @mozaik/themes package

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Mozaik, { Registry } from '@mozaik/ui'
+import Mozaik, { Registry, ThemeManager } from '@mozaik/ui'
+
+// themes
+import { miniKuroTheme, nightBlueTheme, solarizedDarkTheme, wineTheme } from '@mozaik/themes'
+
+// extensions
 import github from '@mozaik/ext-github'
 import travis from '@mozaik/ext-travis'
+
+ThemeManager.add(miniKuroTheme)
+ThemeManager.add(nightBlueTheme)
+ThemeManager.add(solarizedDarkTheme)
+ThemeManager.add(wineTheme)
+
+ThemeManager.defaultTheme = solarizedDarkTheme.name
 
 Registry.addExtensions({
     github,
@@ -10,31 +22,3 @@ Registry.addExtensions({
 })
 
 ReactDOM.render(<Mozaik />, document.getElementById('root'))
-
-/*
-//import solarizedDark from 'mozaik-themes/themes/solarized-dark'
-//import nightBlue     from 'mozaik-themes/themes/night-blue'
-//import sunny         from 'mozaik-themes/themes/sunny'
-//import wine          from 'mozaik-themes/themes/wine'
-//import snow          from 'mozaik-themes/themes/snow'
-//import mini          from 'mozaik-themes/themes/mini'
-//import miniKuro      from 'mozaik-themes/themes/mini-kuro'
-//
-//import 'mozaik-themes/themes/solarized-dark.css'
-//import 'mozaik-themes/themes/night-blue.css'
-//import 'mozaik-themes/themes/sunny.css'
-//import 'mozaik-themes/themes/wine.css'
-//import 'mozaik-themes/themes/snow.css'
-//import 'mozaik-themes/themes/mini.css'
-//import 'mozaik-themes/themes/mini-kuro.css'
-//
-//ThemeManager.add(solarizedDark)
-//ThemeManager.add(nightBlue)
-//ThemeManager.add(sunny)
-//ThemeManager.add(wine)
-//ThemeManager.add(snow)
-//ThemeManager.add(mini)
-//ThemeManager.add(miniKuro)
-//
-//ThemeManager.defaultTheme = nightBlue.name
-*/
---
+++ packages/themes/src/mini-kuro/index.js
@@ -0,0 +1,97 @@
+export default {
+    name: 'mini kuro',
+    colors: {
+        icon: '#fff',
+        unknown: '#c0ab7f',
+        success: '#4eb6a3',
+        warning: '#d1be65',
+        failure: '#ff9176',
+    },
+    root: {
+        fontFamily: `'Roboto Mono', monospace`,
+        color: '#fff',
+        background: '#000',
+        extend: `
+        @import url('https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,500,700');
+        & a {
+            font-weight: 400;
+        }
+        & img {
+            filter: grayscale(100%);
+        }
+        `,
+    },
+    widget: {
+        background: '#000',
+        wrapper: {
+            padding: '1.6vmin 3vmin',
+        },
+        header: {
+            textTransform: 'uppercase',
+            fontSize: '1.8vmin',
+            padding: '0.8vmin 0 0 0',
+            extend: `
+            border-top: 2px solid #fff;
+            align-items: flex-start;    
+            `,
+        },
+    },
+    notifications: {
+        item: {
+            padding: '1.2vmin 2vmin',
+            background: '#fff',
+            color: '#000',
+            extend: `
+            font-weight: 400;
+            `,
+        },
+    },
+    label: {
+        extend: `
+        line-height: 1em;
+        border-top: 1px dotted #fff;
+        border-bottom: 1px dotted #fff;
+        align-items: baseline;
+        `,
+        main: {
+            padding: '1vmin 0.2vmin',
+        },
+        addon: {
+            padding: '1vmin 0.6vmin',
+            extend: `
+            font-weight: 700;
+            &:first-child {
+                padding-right: 1.4vmin;
+                border-right: 1px dotted #fff;
+            }
+            &:last-child {
+                padding-left: 1.4vmin;
+                border-left: 1px dotted #fff;
+            }
+            & + .Label {
+                padding-left: 1.4vmin;
+            }
+            .Label + & {
+                margin-left: 1.4vmin;
+            }
+            `,
+        },
+    },
+    list: {
+        item: {
+            padding: '1vmin 0',
+            extend: `
+            border-bottom: 1px solid #fff;
+            &:last-child {
+                border-bottom: 0;
+            }
+            `,
+            meta: {
+                fontSize: '1.4vmin',
+                extend: `
+                font-weight: 300;
+                `,
+            },
+        },
+    },
+}
