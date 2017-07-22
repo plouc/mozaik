0632f82f24b9ef3b9736e851223eac6469287928
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-07-22 14:58:10 +0900

    feat(themes): add snow theme

--- packages/themes/src/index.js
+++ packages/themes/src/index.js
@@ -1,4 +1,5 @@
 export { default as miniKuroTheme } from './mini-kuro'
 export { default as nightBlueTheme } from './night-blue'
+export { default as snowTheme } from './snow'
 export { default as solarizedDarkTheme } from './solarized-dark'
 export { default as wineTheme } from './wine'
