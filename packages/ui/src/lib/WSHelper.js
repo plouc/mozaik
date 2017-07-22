49906b9aeca7358cc7ad03e2bd563ef145966f94
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-07-22 14:25:28 +0900

    feat(ui): use wss if https is detected

--- packages/ui/src/lib/WSHelper.js
+++ packages/ui/src/lib/WSHelper.js
@@ -1,6 +1,10 @@
 export const guessWSURL = (config = {}) => {
+    if (!window || !window.document || !window.document.location) {
+        throw new Error(`Unable to guess websocket URL because 'window.document.location' is not defined`)
+    }
+
     let proto = 'ws'
-    if (config.useWssConnection === true) {
+    if (config.useWssConnection === true || window.document.location.protocol === 'https:') {
         proto = 'wss'
     }
 
