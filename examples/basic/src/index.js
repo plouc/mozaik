8dbd54b343cc4117f35e78acbfbd7bf2b5532634
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-07-23 10:30:31 +0900

    feat(themes): add mini theme

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,23 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './register_themes'
 import './register_extensions'
-import Mozaik, { ThemeManager } from '@mozaik/ui'
-
-// themes
-import {
-    miniKuroTheme,
-    nightBlueTheme,
-    snowTheme,
-    solarizedDarkTheme,
-    wineTheme,
-} from '@mozaik/themes'
-
-ThemeManager.add(miniKuroTheme)
-ThemeManager.add(nightBlueTheme)
-ThemeManager.add(snowTheme)
-ThemeManager.add(solarizedDarkTheme)
-ThemeManager.add(wineTheme)
-
-ThemeManager.defaultTheme = snowTheme.name
+import Mozaik from '@mozaik/ui'
 
 ReactDOM.render(<Mozaik />, document.getElementById('root'))
