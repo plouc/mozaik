29ed1c92b24c88d683b3a4c4be4fd12ea83a7cf6
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-08-01 11:10:22 +0900

    feat(themes): add support for charts theming

---
+++ packages/themes/src/solarized-dark/charts.js
@@ -0,0 +1,13 @@
+import colors from './colors'
+
+export default {
+    axis: {
+        textColor: colors.text,
+        tickColor: '#085361',
+        legendColor: colors.text,
+    },
+    grid: {
+        stroke:          '#085361',
+        strokeDasharray: '3,6',
+    },
+}
