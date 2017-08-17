1d40f70c3cdbd6e4c8fea4369e0d361adc089782
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-08-17 21:39:57 +0900

    feat(examples): update basic example for travis

--- examples/basic/server.js
+++ examples/basic/server.js
@@ -7,12 +7,11 @@ const Mozaik = require('@mozaik/server')
 
 let configFile = process.argv[2] || 'conf/config.yml'
 
-console.log(`using config file: ${configFile}\n`)
+console.log(`> using config file: '${configFile}'\n`)
 
 Mozaik.configureFromFile(path.join(__dirname, configFile))
     .then(config => {
-        require('./apis')(Mozaik)
-        console.log(config)
+        require('./apis')(Mozaik, configFile, config)
         Mozaik.start()
     })
     .catch(err => {
--- examples/basic/src/register_extensions.js
+++ examples/basic/src/register_extensions.js
@@ -5,15 +5,9 @@ import gitlab from '@mozaik/ext-gitlab'
 import time from '@mozaik/ext-time'
 import travis from '@mozaik/ext-travis'
 
-// third party
-import analytics from 'mozaik-ext-analytics'
-
 Registry.addExtensions({
     github,
     gitlab,
     time,
     travis,
-
-    // third party
-    analytics,
 })
