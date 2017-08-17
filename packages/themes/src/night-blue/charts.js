a612d30fafc693a14bb573be16e376653b60645c
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-08-17 19:02:12 +0900

    feat(themes): extend themes for nivo tooltip

--- packages/themes/src/night-blue/charts.js
+++ packages/themes/src/night-blue/charts.js
@@ -3,11 +3,18 @@ import colors from './colors'
 export default {
     axis: {
         textColor: colors.text,
-        tickColor: '#161f2d',
+        tickColor: colors.text,
         legendColor: colors.text,
     },
     grid: {
         stroke: '#25303d',
     },
     colors: ['#FDECA4', '#cbb04a', '#D68649', '#648e9c', '#253445'],
+    tooltip: {
+        background: '#323f53',
+        color: '#f6ecd0',
+        fontSize: '1.4vmin',
+        borderRadius: '2px',
+        boxShadow: '0 1px 3px rgba(0, 0, 0, 0.5)',
+    },
 }
