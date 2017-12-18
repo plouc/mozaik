d17d876585e83fded2c5372588ece019dc3da9d2
Harry Cummings <harry.cummings@lloydsbanking.com>
2017-12-18 10:34:31 +0000

    Chore: Minor code tidy-ups

--- packages/ui/src/components/widget/table/WidgetTableCell.js
+++ packages/ui/src/components/widget/table/WidgetTableCell.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 const Cell = styled.td`padding: 1vmin 2vmin;`
 
-const WidgetTableCell = ({ children }) =>
-    <Cell>
-        {children}
-    </Cell>
+const WidgetTableCell = ({ children }) => <Cell>{children}</Cell>
+
+WidgetTableCell.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+}
 
 export default WidgetTableCell
