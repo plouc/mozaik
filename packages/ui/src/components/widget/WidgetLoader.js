2830ca4c8e7506fe6155ac12d974c932c2380d03
Harry Cummings <harry.cummings@lloydsbanking.com>
2017-12-18 10:54:38 +0000

    Enhancement: WidgetLoader appears as an animated spinner

--- packages/ui/src/components/widget/WidgetLoader.js
+++ packages/ui/src/components/widget/WidgetLoader.js
@@ -1,19 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-export default class WidgetLoader extends Component {
-    static propTypes = {
-        style: PropTypes.object.isRequired,
-    }
+const WidgetLoader = ({ color }) => (
+    <SpinnerContainer>
+        <StyledSpinner viewBox="0 0 50 50" color={color}>
+            <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="4" />
+        </StyledSpinner>
+    </SpinnerContainer>
+)
 
-    static defaultProps = {
-        style: {},
-    }
-
-    render() {
-        const { style } = this.props
-        const { theme } = this.context
-
-        return <div style={style}>LOADER</div>
-    }
+WidgetLoader.propTypes = {
+    color: PropTypes.string,
 }
+
+const SpinnerContainer = styled.div`
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    overflow: hidden;
+`
+
+const StyledSpinner = styled.svg`
+    animation: rotate 2s linear infinite;
+    height: 50px;
+
+    & .path {
+        stroke: ${props => props.color};
+        stroke-linecap: round;
+        animation: dash 1.5s ease-in-out infinite;
+    }
+
+    @keyframes rotate {
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    @keyframes dash {
+        0% {
+            stroke-dasharray: 1, 150;
+            stroke-dashoffset: 0;
+        }
+        50% {
+            stroke-dasharray: 90, 150;
+            stroke-dashoffset: -35;
+        }
+        100% {
+            stroke-dasharray: 90, 150;
+            stroke-dashoffset: -124;
+        }
+    }
+`
+
+export default WidgetLoader
