2e83b9cce92285bbf969808be9223edc637d7ed0
Raphaël Benitte <raphael.benitte@ekino.com>
2018-07-30 20:26:10 +0900

    fix(ui): fix dashboard player style

--- packages/ui/src/components/dashboard/DashboardPlayer.js
+++ packages/ui/src/components/dashboard/DashboardPlayer.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { MorphIcon, PlayButton as PlayIcon } from 'react-svg-buttons'
 import styled, { withTheme } from 'styled-components'
+import typography from '../../theming/typography'
 
 const Container = styled.div`
     margin-right: 6vmin;
     display: flex;
     align-items: center;
     height: 100%;
+    ${props => typography(props.theme)}
 `
 
 const Button = styled.span`
