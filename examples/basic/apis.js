71aa2c8e1885955acbd65e7aab6391e5ccd6274a
Raphaël Benitte <raphael.benitte@ekino.com>
2018-07-30 21:06:02 +0900

    feat(examples): add json & jira extensions to example

--- examples/basic/apis.js
+++ examples/basic/apis.js
@@ -4,4 +4,6 @@ module.exports = (Mozaik, configFile, config) => {
     Mozaik.registerApi('github', require('@mozaik/ext-github/client'))
     Mozaik.registerApi('travis', require('@mozaik/ext-travis/client'))
     Mozaik.registerApi('gitlab', require('@mozaik/ext-gitlab/client'))
+    Mozaik.registerApi('json', require('@mozaik/ext-json/client'))
+    Mozaik.registerApi('jira', require('@mozaik/ext-jira/client'))
 }
--- examples/basic/src/register_extensions.js
+++ examples/basic/src/register_extensions.js
@@ -2,12 +2,16 @@ import { Registry } from '@mozaik/ui'
 
 import github from '@mozaik/ext-github'
 import gitlab from '@mozaik/ext-gitlab'
+import jira from '@mozaik/ext-jira'
+import json from '@mozaik/ext-json'
 import time from '@mozaik/ext-time'
 import travis from '@mozaik/ext-travis'
 
 Registry.addExtensions({
     github,
     gitlab,
+    jira,
+    json,
     time,
     travis,
 })
