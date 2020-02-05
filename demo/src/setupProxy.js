7ff9be14fa768865ea1116eb85eefb345eeda4f0
Quentin Jadeau <quentin.jadeau@live.fr>
2020-02-05 18:26:19 +0100

    fix: port did not match the config.yml

--- demo/src/setupProxy.js
+++ demo/src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = function (app) {
-  app.use(proxy('/socket', { target: 'http://localhost:5003/', ws: true }))
-  app.use(proxy('/config', { target: 'http://localhost:5003/' }))
+  app.use(proxy('/socket', { target: 'http://localhost:5000/', ws: true }))
+  app.use(proxy('/config', { target: 'http://localhost:5000/' }))
 }
